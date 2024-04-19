f7ee2ccd7b3af8c242e9fa957a553add4ab5074c
sashacherryy <o.vishnewscky14@gmail.com>
2024-04-19 17:51:00 +0300

    whether app v1.0

---
+++ src/components/assets/img.js
@@ -0,0 +1,19 @@
+import search_icon from './search.png'
+import clear_icon from './clear.png'
+import cloud_icon from './cloud.png'
+import drizzle_icon from './drizzle.png'
+import rain_icon from './rain.png'
+import snow_icon from './snow.png' 
+import wind_icon from './wind.png'
+import humidity_icon from './humidity.png'
+
+export {
+    search_icon,
+clear_icon,
+cloud_icon,
+drizzle_icon,
+rain_icon,
+snow_icon,
+wind_icon,
+humidity_icon
+}
